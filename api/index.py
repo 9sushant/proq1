0a6b57368adada5159190ed96d3801756ba233cd
9sushant <156192797+9sushant@users.noreply.github.com>
2025-10-05 08:43:28 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -13,7 +13,6 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all domains to make requests
-    allow_credentials=True,
     allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
 )
